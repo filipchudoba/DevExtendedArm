9ae853e9689941b502dbadcc66d9a7925b77b2b0
Filip Chudoba <80604468+filipchudoba@users.noreply.github.com>
2022-02-25 02:00:56 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import re
 from string import whitespace
+from tracemalloc import stop
+from turtle import end_fill
 
 definedColour = input("Enter colour: ")
 
@@ -9,6 +11,7 @@ class colour:
         self.r = r
         self.g = g
         self.b = b
+        self.finalOperation = "LIGHT_PARAM full_custom_halo_night  0 8.1 -1.9  "
 
     def lightColour(self):
         chosenOneName = self.nazev
@@ -18,8 +21,8 @@ class colour:
         str(chosenOneR)
         str(chosenOneB)
         str(chosenOneR)
-        print("LIGHT_PARAM full_custom_halo_night  0 8.1 -1.9  ",chosenOneR," ",chosenOneG," ",chosenOneB,"1  40 0 -1 0 0")
-
+        self.finalOperation = "LIGHT_PARAM full_custom_halo_night  0 8.1 -1.9  "+str(chosenOneR)+" "+str(chosenOneG)+" "+str(chosenOneB)+" 1  40 0 -1 0 0\n"
+        str(self.finalOperation)
 
 white = colour("white",1,1,1)
 black = colour("black",0,0,0)
@@ -34,19 +37,75 @@ green = colour ("green",0,1,0)
 #Na zaklade inputu vybere prislusnou funkci
 if definedColour == "white": 
     white.lightColour()
+    finalOperation = white.finalOperation
 if definedColour == "black": 
     black.lightColour()
+    finalOperation = black.finalOperation
 if definedColour == "yellow": 
     yellow.lightColour()
+    finalOperation = yellow.finalOperation
 if definedColour == "orange": 
     orange.lightColour()
+    finalOperation = orange.finalOperation
 if definedColour == "red": 
     red.lightColour()
+    finalOperation = red.finalOperation
 if definedColour == "purple": 
     purple.lightColour()
+    finalOperation = purple.finalOperation
 if definedColour == "blue": 
     blue.lightColour()
+    finalOperation = blue.finalOperation
 if definedColour == "cyan": 
     cyan.lightColour()
+    finalOperation = cyan.finalOperation
 if definedColour == "green": 
     green.lightColour()
+    finalOperation = green.finalOperation
+
+
+
+string1 = 'LIGHT_PARAM'
+  
+# opening a text file
+file1 = open("a09_lamp_street2.obj", "r")
+  
+# setting flag and index to 0
+flag = 0
+index = 0
+  
+# Loop through the file line by line
+for line in file1:  
+    index += 1 
+      
+    # checking string is present in line or not
+    if string1 in line:
+        
+      flag = 1
+      break 
+          
+# checking condition for string found or not
+if flag == 0: 
+   print('String', string1 , 'Not Found') 
+else: 
+   print('String', string1, 'Found In Line', index)
+  
+# closing text file    
+file1.close() 
+print (index)
+index -= 1
+
+# with is like your try .. finally block in this case
+with open('a09_lamp_street2.obj', 'r') as file:
+    data = file.readlines()
+
+print ("Changing this line: ", data[index])
+print ("Will be changed with this: " + finalOperation)
+
+data[index] = finalOperation
+
+str(finalOperation)
+
+# and write everything back
+with open('a09_lamp_street2.obj', 'w') as file:
+    file.writelines(data)
